bdca3eac5e44114face863d80d7dcb8851a3dac8
winvis <winvissoftware@gmail.com>
2018-03-08 22:36:46 +0900

    first commit

---
+++ listener.ps1
@@ -0,0 +1,21 @@
+param($port = 8080, $defaulPage = 'index.html')
+$urlRoot = "http://localhost:${port}/"
+$listener = New-Object net.httpListener
+$listener.Prefixes.Add($urlRoot)
+$listener.Start()
+try {
+    while ($true) {
+        $context = $listener.GetContext()
+        $request = $context.Request
+        $request.rawUrl
+        $responce = $context.Response
+        $content = [byte[]]"hello"
+        $responce.OutputStream.Write($content, 0, $content.Length)   
+    }
+        
+}
+catch {
+    
+}finally{
+    $responce.Close()
+}
---
+++ startListener.ps1
@@ -0,0 +1,1 @@
+powershell -ExecutionPolicy RemoteSigned -File .\Listener.ps1
